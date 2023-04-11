3feffd6f1bae8080497fbcd3964e106b74b4cd0c
Alper Karagöz <alperkaragoz@outlook.com>
2023-04-11 16:18:27 +0300

    Add project files.

---
+++ TaskFormApp/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace TaskFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadFile = new System.Windows.Forms.Button();
+            this.rtbBox = new System.Windows.Forms.RichTextBox();
+            this.btnIncrease = new System.Windows.Forms.Button();
+            this.txtCounter = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnReadFile
+            // 
+            this.btnReadFile.Location = new System.Drawing.Point(40, 57);
+            this.btnReadFile.Name = "btnReadFile";
+            this.btnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadFile.TabIndex = 0;
+            this.btnReadFile.Text = "Read File";
+            this.btnReadFile.UseVisualStyleBackColor = true;
+            this.btnReadFile.Click += new System.EventHandler(this.btnReadFile_Click);
+            // 
+            // rtbBox
+            // 
+            this.rtbBox.Location = new System.Drawing.Point(40, 142);
+            this.rtbBox.Name = "rtbBox";
+            this.rtbBox.Size = new System.Drawing.Size(169, 208);
+            this.rtbBox.TabIndex = 1;
+            this.rtbBox.Text = "";
+            // 
+            // btnIncrease
+            // 
+            this.btnIncrease.Location = new System.Drawing.Point(232, 57);
+            this.btnIncrease.Name = "btnIncrease";
+            this.btnIncrease.Size = new System.Drawing.Size(75, 23);
+            this.btnIncrease.TabIndex = 2;
+            this.btnIncrease.Text = "Increase Counter";
+            this.btnIncrease.UseVisualStyleBackColor = true;
+            this.btnIncrease.Click += new System.EventHandler(this.btnIncrease_Click);
+            // 
+            // txtCounter
+            // 
+            this.txtCounter.Location = new System.Drawing.Point(232, 105);
+            this.txtCounter.Name = "txtCounter";
+            this.txtCounter.Size = new System.Drawing.Size(75, 22);
+            this.txtCounter.TabIndex = 3;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(232, 142);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(556, 208);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtCounter);
+            this.Controls.Add(this.btnIncrease);
+            this.Controls.Add(this.rtbBox);
+            this.Controls.Add(this.btnReadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadFile;
+        private System.Windows.Forms.RichTextBox rtbBox;
+        private System.Windows.Forms.Button btnIncrease;
+        private System.Windows.Forms.TextBox txtCounter;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ TaskFormApp/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskFormApp
+{
+    public partial class Form1 : Form
+    {
+        public int counter { get; set; } = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReadFile_Click(object sender, EventArgs e)
+        {
+            string data = string.Empty;
+            Task<string> read = ReadFileAsync();
+
+            richTextBox1.Text = await new HttpClient().GetStringAsync("https://www.google.com");
+
+            // Await ile aşağıdaki kodu yazarak kodu blokluyoruz.(Ana thread i bloklamadan.)
+            // Eğer ilgili thread i o anda sonlandırıp datayı almak istiyorsan await keywordünü kullanıyoruz.Örn: data=await ReadFileAsync(); Eğer arada işlemler yapıp sonrasında datayı almak istiyorsak aşağıdaki şekilde data=await read; olarak kullanıyoruz.
+            data = await read;
+
+            rtbBox.Text = data;
+        }
+
+        private void btnIncrease_Click(object sender, EventArgs e)
+        {
+            txtCounter.Text = counter++.ToString();
+        }
+
+        private string ReadFile()
+        {
+            string data = string.Empty;
+            using (StreamReader sr = new StreamReader("file.txt"))
+            {
+                // Ana thread i bloklar(Thread.Sleep()).
+                Thread.Sleep(5000);
+                data = sr.ReadToEnd();
+            }
+            return data;
+        }
+
+        private async Task<string> ReadFileAsync()
+        {
+            string data = string.Empty;
+            using (StreamReader sr = new StreamReader("file.txt"))
+            {
+                Task<string> task = sr.ReadToEndAsync();
+
+                // data=await task; koduna gelene kadar .ReadToEndAsync() metotundan dönecek datadan bağımsız işler yapılabilir.
+
+                // İlgili thread i bloklar.(Task.Delay)
+                await Task.Delay(1000);
+
+                data = await task;
+            }
+            return data;
+        }
+    }
+}
